db13f6fbb98c3d52ca2eb8fa0b1c5bea137bbae8
Robert <robert@canyonechostudios.com>
2026-05-03 06:34:14 -0600

    Tests

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -10,3 +10,5 @@ class Menu(models.Model):
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.IntegerField(5)
+    def __str__(self):
+        return f'{self.Title} : {str(self.Price)}'
