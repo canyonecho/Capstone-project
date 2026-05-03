25ee2fe1003170d1c4097270c6ac573a2513965d
Robert <robert@canyonechostudios.com>
2026-05-03 07:29:15 -0600

    Readme

---
+++ LittleLemon/Readme.txt
@@ -0,0 +1,6 @@
+Api's 
+
+auth/users/ 
+auth/token/login
+/menu/ 
+/booking/tables/
--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -3,12 +3,12 @@ from django.utils import timezone
 
 class Booking(models.Model):
     Name = models.CharField(max_length=255)
-    No_of_guests = models.IntegerField(6)
+    No_of_guests = models.IntegerField(default=6)
     BookingDate = models.DateField(default=timezone.now())
 
 class Menu(models.Model):
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
-    Inventory = models.IntegerField(5)
+    Inventory = models.IntegerField(default=5)
     def __str__(self):
         return f'{self.Title} : {str(self.Price)}'
